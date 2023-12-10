3c013bb7535dcc034b01f8be1b32ca6f9a42b0c3
H0onnn <dukei2017@naver.com>
2023-12-10 14:17:20 +0900

    Refactor: fix useCallback

--- debounce-ts/src/components/DebounceWitUseCallback.tsx
+++ debounce-ts/src/components/DebounceWitUseCallback.tsx
@@ -7,38 +7,49 @@ const DebounceWithUseCallback = () => {
   // eslint-disable-next-line
   const [buttonClickCount, setButtonClickCount] = useState<number>(0);
 
-  const debouncedInputValue = useCallback(() => {
-    const { debounced, cancel } = debounce((debouncedValue: string) => {
+  const debouncedInputValue = useCallback(
+    debounce((debouncedValue: string) => {
       console.log("디바운싱 input", debouncedValue);
-    });
+    }).debounced,
+    []
+  );
 
-    return { debounced, cancel };
-  }, []);
-
-  const debouncedButtonClick = useCallback(() => {
-    const { debounced, cancel } = debounce(
+  // 버튼 클릭에 대한 디바운싱 함수
+  const debouncedButton = useCallback(
+    debounce(
       () => {
         setButtonClickCount((prevCount) => prevCount + 1);
         console.log("디바운싱 button");
       },
       1000,
       true
-    );
+    ).debounced,
+    []
+  );
 
-    return { debounced, cancel };
-  }, []);
+  // 버튼 클릭 디바운싱 취소 함수
+  const cancelButtonDebounce = useCallback(
+    debounce(
+      () => {
+        console.log("디바운싱 button cancel");
+      },
+      1000,
+      true
+    ).cancel,
+    []
+  );
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-    debouncedInputValue().debounced(e.target.value);
+    debouncedInputValue(e.target.value);
   };
 
   const handleButtonClick = () => {
-    debouncedButtonClick().debounced();
+    debouncedButton();
   };
 
   const handleCancelDebouncedButton = () => {
-    debouncedButtonClick().cancel();
+    cancelButtonDebounce();
   };
 
   return (
