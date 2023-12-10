3dba8091e7cbbd47daa64027c1914cc43c4e208f
H0onnn <dukei2017@naver.com>
2023-12-10 11:43:08 +0900

    Feat: App 컴포넌트에 각 예제 적용

--- debounce-ts/src/App.tsx
+++ debounce-ts/src/App.tsx
@@ -1,7 +1,24 @@
+import DebounceExample from "./components/DebounceExample";
+import DebounceWithUseCallback from "./components/DebounceWitUseCallback";
+import DebounceWithRef from "./components/DebounceWithRef";
+import DebounceWithHooks from "./components/DebounceWithHooks";
+
 function App() {
   return (
     <div className="App">
-      <h1>Debounce</h1>
+      <h1
+        style={{
+          fontSize: "20px",
+          textAlign: "center",
+          margin: "18px 0",
+        }}
+      >
+        디바운스 함수 구현하기
+      </h1>
+      <DebounceExample />
+      <DebounceWithUseCallback />
+      <DebounceWithRef />
+      <DebounceWithHooks />
     </div>
   );
 }
